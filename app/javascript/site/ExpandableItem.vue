1d82975f5a143d7f1f2c86dbdc01b83d3af78e46
Will Kocur <will.kocur@unep-wcmc.org>
2019-01-16 14:45:50 +0000

    basic text styles as template

---
+++ app/javascript/site/ExpandableItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="expandable-item relative" :class="{'expandable-item--active': isActive}">
+    <div 
+      class="expandable-item__header hover--pointer"
+      @click="toggle">
+      <slot name="header"></slot>
+      <i class="select__drop-arrow arrow-svg"/>
+    </div>
+    <div v-if="isActive" class="expandable-item__container item-padding">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    title: String
+  },
+  data: function () {
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    toggle () {
+      this.isActive = !this.isActive;
+    }
+  }
+}
+</script>
--- app/javascript/vue.js
+++ app/javascript/vue.js
@@ -1,13 +1,13 @@
 import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue/dist/vue.esm'
 
-import Test from './components/Test'
+import ExpandableItem from './site/ExpandableItem'
 
 Vue.use(TurbolinksAdapter)
 
 document.addEventListener('turbolinks:load', () => {
   const app = new Vue({
     el: '#v-app',
-    components: { Test }
+    components: { ExpandableItem }
   })
 })
