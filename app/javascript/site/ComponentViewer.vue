ec74faf32dff6360b9b007dfaa728b2a8abf76b5
Will Kocur <will.kocur@unep-wcmc.org>
2019-02-19 16:47:04 +0000

    add some aria

--- app/javascript/site/ComponentViewer.vue
+++ app/javascript/site/ComponentViewer.vue
@@ -3,13 +3,15 @@
   <aside class="filters flex-1-quarter">
     <ul class="filters__list ul--unstyled">
       <li v-for="category in categories" 
-        :class="{'filters__list-item--selected': isSelectedCategory(category.id)}"
-        @click="clickCategory(category.id)">
-        <button class="filters__list-item sg-h3 hover--pointer">{{ category.name }}</button>
+        :class="{'filters__list-item--selected': isSelectedCategory(category.id)}">
+        <button
+          aria-controls="component-grid"
+          class="filters__list-item sg-h3 hover--pointer"
+          @click="clickCategory(category.id)">{{ category.name }}</button>
       </li>
     </ul>
   </aside>
-  <main class="flex-3-quarters">
+  <main id="component-grid" class="flex-3-quarters">
     <ul class="component-grid ul--unstyled page-container">
       <li v-for="comp in components" v-if="belongsToSelectedCategory(comp)" class="component-grid__element sg-h3">
         <a :href="getComponentPath(comp.id)">{{ comp.name }}</a>
