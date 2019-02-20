4e619eef8df2cc7d1c056f407fe27f74c4304f26
Will Kocur <will.kocur@unep-wcmc.org>
2019-02-20 14:03:16 +0000

    updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,7 @@ set :pty, true
 # Default value for :linked_files is []
 #set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
-set :linked_files, %w{config/database.yml .env} 
+set :linked_files, %w{config/database.yml .env config/master.key} 
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'node_modules', 'client/node_modules')
@@ -64,5 +64,3 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :keep_releases, 5
 
 set :passenger_restart_with_touch, false
-
-set :linked_files, %w{config/master.key}
