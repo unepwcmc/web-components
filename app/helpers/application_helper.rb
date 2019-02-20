ff33815d507e522341afbef263c9eeef99760212
Will Kocur <will.kocur@unep-wcmc.org>
2019-02-20 17:56:03 +0000

    style nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   end
 
   def nav_link_class(path)
-    link_class = "nav__link " 
-    link_class += current_uri == path ? "nav__link--current" : ''
+    link_class = "site-nav__link " 
+    link_class += current_uri == path ? "site-nav__link--current" : ''
   end
 end
