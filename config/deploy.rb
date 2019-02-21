f894c98ccdd2b4e5e9079d80b32365a8f94f0821
Will Kocur <will.kocur@unep-wcmc.org>
2019-02-21 16:32:10 +0000

    don't link node_modules

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,6 @@ set :nvm_type, :user # or :system, depends on your nvm setup
 set :nvm_node, 'v9.0.0'
 set :nvm_map_bins, %w{node npm yarn}
 
-
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
@@ -53,7 +52,7 @@ set :pty, true
 set :linked_files, %w{config/database.yml .env config/master.key} 
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'node_modules', 'client/node_modules')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 
 # Default value for default_env is {}
