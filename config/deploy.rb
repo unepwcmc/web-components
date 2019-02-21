a2158c9b04bd33ba00ddc693af0fa4912380b878
Will Kocur <will.kocur@unep-wcmc.org>
2019-02-21 16:40:03 +0000

    reset changes - no luck

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,7 +52,7 @@ set :pty, true
 set :linked_files, %w{config/database.yml .env config/master.key} 
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'node_modules', 'client/node_modules')
 
 
 # Default value for default_env is {}
