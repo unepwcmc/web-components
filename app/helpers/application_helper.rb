ec2d6086eef586c32b1af87c8acd6e26c4472107
Will Kocur <will.kocur@unep-wcmc.org>
2019-02-27 11:56:17 +0000

    get stylguide from params and use to wrap styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
     link_class = "site-nav__link " 
     link_class += current_uri == path ? "site-nav__link--current" : ''
   end
+
+  def styleguide_class
+    "styleguide-#{params[:styleguide] || 'site'}"
+  end
 end
