379440368e61439f7c2ee194c9d3ca473d6d9069
Stuart Watson <stuart.watson@unep-wcmc.org>
2019-03-18 14:27:05 +0000

    New staging deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,8 @@
 set :stage, :staging
-set :branch, "develop"
+set :branch, "supported-staging"
 
 
-server "web-staging.linode.unep-wcmc.org", user: 'wcmc', roles: %w{app web db}
+server "web-supported-staging.linode.unep-wcmc.org", user: 'wcmc', roles: %w{app web db}
 
 set :application, "style-guide"
 set :server_name, "style-guide"
