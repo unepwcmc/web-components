3e7f2133996c356652f5eb605b817cef7fb4e987
Will Kocur <will.kocur@unep-wcmc.org>
2019-07-08 20:08:15 +0100

    typo

--- app/javascript/component-lib/mixins/mixin-ids.js
+++ app/javascript/component-lib/mixins/mixin-ids.js
@@ -4,7 +4,7 @@ export default {
       return `${this.$options._componentTag}-${this._uid}`
     },
     getVForKey (elementName, index) {
-      return `${this.getComponentId}-${elementName}-${index}`
+      return `${this.getComponentId()}-${elementName}-${index}`
     }
   }
 }
