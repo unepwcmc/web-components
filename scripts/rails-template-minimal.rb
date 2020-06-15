d742e8da93bba34d654438adc80b1d73c717ffd0
Stanley Liu <53650048+stanleypliu@users.noreply.github.com>
2020-06-15 16:51:03 +0100

    Added rails templates into /scripts

---
+++ scripts/rails-template-minimal.rb
@@ -0,0 +1,151 @@
+run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
+
+# GEMFILE
+########################################
+inject_into_file 'Gemfile', before: 'group :development, :test do' do
+  <<~RUBY
+    gem 'autoprefixer-rails'
+    gem 'font-awesome-sass'
+    gem 'simple_form'
+
+  RUBY
+end
+
+inject_into_file 'Gemfile', after: 'group :development, :test do' do
+  <<-RUBY
+  gem 'capistrano', '~> 3.14.0', require: false
+  gem 'capistrano-rails',   '~> 1.5.0', require: false
+  gem 'capistrano-bundler', '~> 1.6.0', require: false
+  gem 'capistrano-rvm', '~> 0.1.2', require: false
+  gem 'capistrano-passenger', '~> 0.2.0', require: false
+
+
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  gem 'dotenv-rails'
+
+  RUBY
+end
+
+# Procfile
+########################################
+file 'Procfile', <<~YAML
+  web: bundle exec puma -C config/puma.rb
+YAML
+
+# Assets - very minimal folder structure
+########################################
+run 'rm -rf app/assets/stylesheets/*'
+run 'cd app/assets && mkdir fonts'
+run 'cd app/assets/stylesheets && mkdir base components pages utilities && touch application.scss'
+
+# Dev environment
+########################################
+gsub_file('config/environments/development.rb', /config\.assets\.debug.*/, 'config.assets.debug = false')
+
+# Layout - includes responsive support and Webpacker integration
+########################################
+if Rails.version < "6"
+  scripts = <<~HTML
+    <%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload', defer: true %>
+        <%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %>
+  HTML
+  gsub_file('app/views/layouts/application.html.erb', "<%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload' %>", scripts)
+else
+  gsub_file('app/views/layouts/application.html.erb', "<%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %>", "<%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload', defer: true %>")
+end
+
+gsub_file('app/views/layouts/application.html.erb', "<%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %>", "<%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload', defer: true %>")
+style = <<~HTML
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>
+HTML
+gsub_file('app/views/layouts/application.html.erb', "<%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>", style)
+
+
+# Generators
+########################################
+generators = <<~RUBY
+  config.generators do |generate|
+    generate.assets false
+    generate.helper false
+    generate.test_framework :test_unit, fixture: false
+  end
+RUBY
+
+environment generators
+
+########################################
+# AFTER BUNDLE
+########################################
+after_bundle do
+  # Generators: db + simple form + pages controller
+  ########################################
+  rails_command 'db:drop db:create db:migrate'
+  generate('simple_form:install', '--bootstrap')
+  generate(:controller, 'pages', 'home', '--skip-routes', '--no-test-framework')
+
+  # Routes
+  ########################################
+  route "root to: 'pages#home'"
+
+  # Git ignore
+  ########################################
+  append_file '.gitignore', <<~TXT
+    # Ignore .env file containing credentials.
+    .env*
+
+    # Ignore Mac and Linux file system files
+    *.swp
+    .DS_Store
+  TXT
+
+  # Webpacker / Yarn
+  ########################################
+  run 'yarn add popper.js jquery bootstrap'
+  append_file 'app/javascript/packs/application.js', <<~JS
+
+    // External imports
+    import "bootstrap";
+
+    // Internal imports, e.g:
+    // import { initSelect2 } from '../components/init_select2';
+
+    document.addEventListener('turbolinks:load', () => {
+      // Call your functions here, e.g:
+      // initSelect2();
+    });
+  JS
+
+  inject_into_file 'config/webpack/environment.js', before: 'module.exports' do
+    <<~JS
+      const webpack = require('webpack');
+
+      // Preventing Babel from transpiling NodeModules packages
+      environment.loaders.delete('nodeModules');
+
+      // Bootstrap 4 has a dependency over jQuery & Popper.js:
+      environment.plugins.prepend('Provide',
+        new webpack.ProvidePlugin({
+          $: 'jquery',
+          jQuery: 'jquery',
+          Popper: ['popper.js', 'default']
+        })
+      );
+
+    JS
+  end
+
+  # Dotenv
+  ########################################
+  run 'touch .env'
+
+
+  # Git
+  ########################################
+  git add: '.'
+  git commit: "-m 'Initial commit with minimal WCMC template'"
+
+  # Fix puma config
+  gsub_file('config/puma.rb', 'pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }', '# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }')
+end
