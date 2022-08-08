35d0f3223b6a9b04e90cb547756a12e3fc321d66
Joseph Burke <joseph_burke@outlook.com>
2022-08-08 13:19:28 +0200

    refactor: have Filterable use ActiveSupport::Concern

--- wcmc_components/lib/wcmc_components/filterable.rb
+++ wcmc_components/lib/wcmc_components/filterable.rb
@@ -5,23 +5,15 @@ require 'wcmc_components/filterable/serializer'
 
 module WcmcComponents
   module Filterable
-    def self.included(base)
-      base.extend ClassMethods
-      base.include InstanceMethods
+    extend ActiveSupport::Concern
 
-      # The following block is run in the context of the class where Filterable is included
-      # The instance variables set here belong to that base class.
-      base.instance_eval do
-        # @table_attributes stores information about which columns in the DB are to be included in the FilterableTable
-        class_attribute :table_attributes, default: TableAttributes.new
-      end
-    end
+    included do
+      class_attribute :table_attributes, default: Attributes.new
 
-    module InstanceMethods
       delegate :attributes_for_table, to: :table_attributes
       # Gets the objects attributes and transforms them into one table row for the FilterableTable component
       def as_table_row
-        TableSerializer.convert_item_to_table_row(self)
+        Serializer.convert_item_to_table_row(self)
       end
 
       # table_page_path returns the 'show' path for the resource
@@ -30,7 +22,7 @@ module WcmcComponents
       end
     end
 
-    module ClassMethods
+    class_methods do
       # table_attribute is a wrapper for TableAttributes#add_attribute
       # Use this in the class definition to add an attribute
       def table_attribute(name, **options)
@@ -39,9 +31,9 @@ module WcmcComponents
 
       # The primary entrypoint method for serializing items of the base class for use in the FilterableTable component
       def paginate_for_table(**table_parameter_options)
-        table_parameters = TableParameters.new(table_parameter_options)
-        query_results = TableQueryObject.new(self).query_with_table_parameters(table_parameters)
-        TableSerializer.serialize_relation_for_table(query_results, table_parameters)
+        table_parameters = Parameters.new(table_parameter_options)
+        query_results = QueryObject.new(self).query_with_table_parameters(table_parameters)
+        Serializer.serialize_relation_for_table(query_results, table_parameters)
       end
 
       def columns_to_json
