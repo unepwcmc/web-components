deb5fd5f935b7f349945f25591e7f0d090fd5524
maxime <maxycle@hotmail.com>
2022-10-05 10:10:36 +0100

    fix: add path name

--- wcmc_components/config/routes.rb
+++ wcmc_components/config/routes.rb
@@ -1,7 +1,7 @@
 WcmcComponents::Engine.routes.draw do
   scope module: 'table' do
     post '/', action: 'index'
-    get '/:id/edit', action: 'edit'
+    get '/:id/edit', action: 'edit', as: :edit
     put '/:id/update', action: 'update'
 
     scope controller: 'download' do
