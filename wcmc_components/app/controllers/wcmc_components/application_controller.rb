18f46cf6d2f3144df9289a3d17346a555fa677da
Joseph Burke <joseph_burke@outlook.com>
2022-10-21 11:38:24 +0200

    fix: uncomment forgery protection
    Commenting this line allows API calls from Postman etc. for dev purposes
    Shouldn't make it to `develop`

--- wcmc_components/app/controllers/wcmc_components/application_controller.rb
+++ wcmc_components/app/controllers/wcmc_components/application_controller.rb
@@ -1,6 +1,6 @@
 module WcmcComponents
   class ApplicationController < ActionController::Base
-    # protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
     skip_before_action :verify_authenticity_token
 
     private
