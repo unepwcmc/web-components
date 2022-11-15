0260dee363eeaa626fda5f805def195542fe5bc3
Will Kocur <willkocur@gmail.com>
2022-11-15 20:23:39 +0000

    refactor: remove accidental whitespace

--- wcmc_devise_sso/lib/generators/wcmc_devise_sso_generator.rb
+++ wcmc_devise_sso/lib/generators/wcmc_devise_sso_generator.rb
@@ -9,7 +9,7 @@ RUBY
 CREATE_FROM_PROVIDER_DATA_METHOD = <<-RUBY
 
   def self.create_from_provider_data(provider_data)
-    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create  do |user|
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
       user.email = provider_data.info.email
       user.password = Devise.friendly_token[0, 20]
       user.wcmc!
